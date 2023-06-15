8cd326d8dcaa024317351da7e88d47b2682d948f
percival001 <116484345+percival001@users.noreply.github.com>
2023-06-15 11:23:10 +0700

    abc

---
+++ pds_project/book.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500&display=swap" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <?php
+    // include 'connect.php';
+    session_start();
+    ?>
+    <style>
+        body {
+            background-color: #1b262c;
+        }
+
+        h1,
+        h2,
+        h3,
+        h4,
+        h5,
+        h6 {
+            color: #ffffff;
+        }
+
+        .jadwal {
+            border: none;
+            background-image: linear-gradient(to right, #3282B8, #BBE1FA);
+        }
+
+        .jumbotron {
+            padding: 2rem;
+            background-color: #e9ecef;
+
+        }
+
+        #spec{
+            font-size: 6.5em;
+        }
+
+        
+
+    </style>
+</head>
+
+<body>
+    <?php
+    $connectionString = "mongodb://localhost:27017";
+    $databaseName = "clinic";
+    $collectionName = "dokter";
+
+    $manager = new MongoDB\Driver\Manager($connectionString);
+
+    $query = new MongoDB\Driver\Query([]);
+
+    $database = $databaseName;
+    $collection = $collectionName;
+
+    $cursor = $manager->executeQuery("$database.$collection", $query);
+
+    $spesialis = [];
+    $nama_dokter = [];
+    $jadwal = [];
+
+    // Masukkan ke array spesialis
+    foreach ($cursor as $document) {
+        // Spesialis
+        array_push($spesialis, $document->spesialis);
+        // Nama dokter
+        $fieldObject = $document->biodata;
+        $nestedField = $fieldObject->name;
+        array_push($nama_dokter, $nestedField);
+        // Jadwal
+        array_push($jadwal, $document->jadwal);
+    }
+    $q=0;
+    // Echo spesialis dan dokternya
+    for ($i = 0; $i < count($spesialis); $i++) {
+        // ambil nama spesialis
+        $nama_spesialis = $spesialis[$i];
+        $temp = [];
+        if ($nama_spesialis != 'done') {
+            $q++;
+            if($q%2==0){
+                echo "<div class='row g-0'><div id='pengisi' class='col-lg-5 d-flex align-items-center justify-content-center' style='background-color:#BBE1FA'><h1 id='spec' class='text-center' style='color:#0F4C75'><b> Sp. " . ucwords($nama_spesialis) . "</b></h1></div><div id='keterangan' class='col-lg-7'>";
+                echo "<div class='jumbotron' style='background-color:#0F4C75'>";
+            }
+            else{
+                echo "<div class='row g-0'><div id='pengisi' class='col-lg-5 d-flex align-items-center justify-content-center' style='background-color:#3282B8'><h1 id='spec' class='text-center'><b>Sp. " . ucwords($nama_spesialis) . "</b></h1></div><div id='keterangan' class='col-lg-7'>";
+                echo "<div class='jumbotron' style='background-color:#0F4C75'>";
+            }
+            
+            for ($j = 0; $j < count($spesialis); $j++) { // cari yang sama dengan $nama_spesialis
+                if ($spesialis[$j] == $nama_spesialis && $spesialis[$j] != 'done') {
+                    array_push($temp, $j);
+                    $spesialis[$j] = 'done';
+                }
+            }
+
+            for ($k = 0; $k < count($temp); $k++) { //echo semua yg diperlukan di dokter itu
+                echo "<div class='row g-0'><div class='col-lg-6'>";
+                echo "<img src='https://drive.google.com/uc?export=view&id=1dAEbPNhBnXC1wU16elJYHfIhPlirR-77' width='300' height='380'>";
+                echo "</div>";
+                echo "<div class='col-lg-6'>";
+                // Echo nama dokter
+                echo "<br><h1>" . $nama_dokter[$temp[$k]] . "</h1><br>";
+                echo "<h5>Schedule : </h5>";
+                // Echo jadwal dokter
+                for ($l = 0; $l < count($jadwal[$temp[$k]]); $l++) {
+                    $tempp = $jadwal[$temp[$k]];
+                    $modalId = "exampleModal" . $temp[$k] . $l; // Generate unique modal id
+                    echo "<button type='button' class='btn btn-primary btn-lg jadwal' data-bs-toggle='modal' data-bs-target='#$modalId'>" . $tempp[$l] . "</button>";
+                    echo '<br><br>';
+                    // Modal content
+                    echo "<div class='modal fade' id='$modalId' tabindex='-1' role='dialog' aria-labelledby='exampleModalLabel' aria-hidden='true'>";
+                    echo "<div class='modal-dialog' role='document'>";
+                    echo "<div class='modal-content' style='background-color:#0F4C75'>";
+                    echo "<div class='modal-header'>";
+                    echo "<h5 class='modal-title' id='exampleModalLabel'>Please confirm your appointment</h5>";
+                    echo "<button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'>";
+                    echo "</button>";
+                    echo "</div>";
+                    echo "<div class='modal-body'>";
+                    echo "<h2>" . $nama_dokter[$temp[$k]] . "</h2>";
+                    echo "<h5>Specializes in: " . $nama_spesialis . "</h5>";
+                    echo "<br><h6>Time: " . $tempp[$l] . "</h6>";
+                    echo "</div>";
+                    echo "<div class='modal-footer'>";
+                    echo "<button type='button' class='btn btn-danger' data-bs-dismiss='modal'>Cancel</button>";
+                    echo "<button type='button' class='btn btn-primary'>Confirm</button>";
+                    echo "</div>";
+                    echo "</div>";
+                    echo "</div>";
+                    echo "</div>";
+                }
+                echo "<br><br>";
+                echo "</div></div><br><br>"; // Close the row and col-lg-3 divs
+            }
+
+            echo "</div></div></div>"; // Close the jumbotron div
+        }
+    }
+    ?>
+
+</body>
+
+</html>
---
+++ pds_project/booking.php
@@ -0,0 +1,148 @@
+<?php
+require '../vendor/autoload.php';
+
+use MongoDB\Driver\Manager;
+use MongoDB\Driver\Query;
+use MongoDB\BSON\Regex;
+
+// Connection settings
+$connectionString = "mongodb://localhost:27017";
+
+// Create a manager instance
+$manager = new Manager($connectionString);
+
+// Specify the database and collection
+$database = "forum_db";
+$collection = "collection_db";
+
+// Function to fetch and filter documents from the collection
+function fetchFilteredDocuments($manager, $database, $collection, $filter)
+{
+    // Create a query object with the filter
+    $query = new Query($filter);
+
+    // Execute the query
+    $cursor = $manager->executeQuery("$database.$collection", $query);
+
+    // Fetch and return documents as an array
+    return iterator_to_array($cursor);
+}
+
+// Function to fetch all documents from the collection
+function fetchAllDocuments($manager, $database, $collection)
+{
+    // Create an empty filter to retrieve all documents
+    $filter = [];
+
+    // Create a query object with the filter
+    $query = new Query($filter);
+
+    // Execute the query
+    $cursor = $manager->executeQuery("$database.$collection", $query);
+
+    // Fetch and return documents as an array
+    return iterator_to_array($cursor);
+}
+
+// Handle form submission
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Get the filter values from the form
+    $user = $_POST['user'];
+    $thread = $_POST['thread'];
+    $tags = $_POST['tags'];
+
+    // Create regex patterns to perform partial matches on the filter values
+    $userPattern = new Regex($user, 'i');
+    $threadPattern = new Regex($thread, 'i');
+    $tagPattern = new Regex($tags, 'i');
+
+    // Construct the filter
+    $filter = [];
+
+    if (!empty($user)) {
+        $filter['user'] = $userPattern;
+    }
+
+    if (!empty($thread)) {
+        $filter['thread'] = $threadPattern;
+    }
+
+    if (!empty($tags)) {
+        $filter['tags'] = ['$in' => [$tagPattern]];
+    }
+
+    // Fetch filtered documents from the collection
+    $documents = fetchFilteredDocuments($manager, $database, $collection, $filter);
+} elseif (isset($_POST['showAll'])) {
+    // Fetch all documents from the collection
+    $documents = fetchAllDocuments($manager, $database, $collection);
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+</head>
+
+<body>
+    <!-- Filter form -->
+    <form method="POST">
+        <!-- Show -->
+        <button type="submit" name="showAll">Show all</button>
+        <br><br>
+        <!-- User -->
+        <label for="user">User:</label>
+        <input type="text" name="user" id="user" placeholder="Input user..">
+
+        <!-- Thread -->
+        <label for="thread">Thread:</label>
+        <input type="text" name="thread" id="thread" placeholder="Input thread..">
+
+        <!-- Tags -->
+        <label for="tags">Tag:</label>
+        <input type="text" name="tags" id="tags" placeholder="Input tags..">
+
+        <button type="submit" name="filterData">Filter</button>
+
+    </form>
+
+    <!-- Display filtered documents -->
+    <?php if (isset($documents)) : ?>
+        <br>
+        <h3>Result(s) : </h3>
+        <table border='1' style='border-collapse:collapse'>
+            <thead>
+                <tr>
+                    <th>Property</th>
+                    <th>Value</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($documents as $document) :
+                    foreach ($document as $property => $value) :
+                        if (is_array($value)) :
+                            foreach ($value as $index => $item) : ?>
+                                <tr>
+                                    <td><?php echo "{$property}[{$index}]"; ?></td>
+                                    <td><?php echo $item; ?></td>
+                                </tr>
+                            <?php endforeach; ?>
+                        <?php else : ?>
+                            <tr>
+                                <td><?php echo $property; ?></td>
+                                <td><?php echo $value; ?></td>
+                            </tr>
+                    <?php endif;
+                    endforeach; ?>
+                    <tr>
+                        <td colspan="2">&nbsp;</td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    <?php endif; ?>
+</body>
+
+</html>+
