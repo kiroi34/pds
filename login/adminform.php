ee0b4bb3a5c580c2e782c3097e3f23b9b93e8920
steffan1501 <116496816+steffan1501@users.noreply.github.com>
2023-06-16 22:17:37 +0700

    database sql

--- login/adminform.php
+++ login/adminform.php
@@ -95,6 +95,7 @@
     </style>
 </head>
 <body>
+
     <h2>Admin Login</h2>
     <form action="adminlogin.php" method="POST">
         <label for="username">Username:</label>
@@ -108,5 +109,17 @@
         <h5>Doesn't have an account? </h5> <a href="adminsignupform.php" style="margin: 0 auto; display: block; font-weight: bold; font-size: 13px" >Click Here</a>
 
     </form>
+     <!-- Javascript files-->
+     <script src="js/jquery.min.js"></script>
+      <script src="js/popper.min.js"></script>
+      <script src="js/bootstrap.bundle.min.js"></script>
+      <script src="js/jquery-3.0.0.min.js"></script>
+      <script src="js/plugin.js"></script>
+      <!-- sidebar -->
+      <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
+      <script src="js/custom.js"></script>
+      <!-- javascript --> 
+      <script src="js/owl.carousel.js"></script>
+      <script src="https:cdnjs.cloudflare.com/ajax/libs/fancybox/2.1.5/jquery.fancybox.min.js"></script>
 </body>
 </html>
